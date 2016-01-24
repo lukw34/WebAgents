f14b3dc42f6bb6813e7efd63832a9a8aca12f629
ukasz <lukw34@gmail.com>
2016-01-24 20:28:31 +0100

    get unprocessed data from page

--- src/WebAgent.java
+++ src/WebAgent.java
@@ -1,7 +1,25 @@
+import com.jaunt.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebAgent {
 
     public static void main(String[] args) {
 
+        try{
+            UserAgent userAgent = new UserAgent();
+            userAgent.visit("http://lowcygier.pl/sledzokazje/?Price_sort=change");
+            userAgent.doc.findEach("<tr class=").forEach(game -> {
+                System.out.println(game);
+                game.getChildNodes().forEach(System.out::print);
+            });
+
+
+        }
+        catch(JauntException e){
+            System.err.println(e);
+        }
     }
 }
